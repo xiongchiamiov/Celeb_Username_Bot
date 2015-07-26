4cfce78d9538a08154affb30e8776f36608f03e1
DrewsephA <drewlallen@gmail.com>
2015-07-26 19:04:38 -0400

    config.py
    This is the list of celebs the bot currently identifies. As new ones are discovered, they will be added here.

---
+++ config.py
@@ -0,0 +1,184 @@
+''' ---------------------------------------------------------------------------------------------------------------- '''
+''' These below are the membership groups and their membership customized replies. Edit this area to expand the bot. '''
+''' ---------------------------------------------------------------------------------------------------------------- '''
+
+SelenaGomez = ("selena", "gomez") #Will trigger if these two words BOTH are in the title.
+SelenaGomezReply = """
+[Instagram](https://instagram.com/SelenaGomez/)\n
+[Twitter](https://twitter.com/SelenaGomez)\n
+"""
+
+AlexisRen = ("alexis", "ren") #Every ("firstname", "nickname", "lastname") in the ()'s below must be lower case.
+AlexisRenReply = """
+[Instagram](https://instagram.com/alexisren)\n
+[Twitter](https://twitter.com/alexisreng)\n
+[Tumblr](http://alexisreneg.tumblr.com)\n
+"""
+
+TaylorSwift = ("taylor", "swift")
+TaylorSwiftReply = """
+[Instagram](https://instagram.com/taylorswift)\n
+[Twitter](https://twitter.com/Taylorswift13)\n
+[Facebook](https://www.facebook.com/TaylorSwift)\n
+[Website](http://www.taylorswift.com/)\n
+"""
+
+McKaylaMaroney = ("mckayla", "maroney")
+McKaylaMaroneyReply = """
+[Instagram](https://instagram.com/McKaylaMaroney)\n
+[Twitter](https://twitter.com/mckaylamaroney)\n
+[YouTube Channel](https://www.youtube.com/channel/UC0HJyx19LKRmuHxfiqp9E8w)\n
+[Keek](https://www.keek.com/profile/McKaylaMaroney)\n
+[Vine](https://vine.co/u/920773070459904000)\n
+"""
+
+SarahHyland = ("sarah", "hyland")
+SarahHylandReply = """
+[Instagram](https://instagram.com/therealsarahhyland)\n
+[Twitter](https://twitter.com/sarah_hyland)\n
+"""
+
+ArielWinter = ("ariel", "winter")
+ArielWinterReply = """
+[Instagram](https://instagram.com/arielwinter)\n
+[Twitter](https://twitter.com/arielwinter1)\n
+"""
+
+KarlieKloss = ("karlie", "kloss")
+KarlieKlossReply = """
+[Instagram](https://instagram.com/karliekloss/)\n
+[Twitter](https://twitter.com/karliekloss/)\n
+[YouTube](https://www.youtube.com/c/karliekloss)\n
+[Facebook](https://www.facebook.com/KarlieKloss)\n
+"""
+
+KendallJenner = ("kendall", "jenner")
+KendallJennerReply = """
+[Instagram](https://instagram.com/kendalljenner)\n
+[Twitter](https://twitter.com/kendalljenner)\n
+[Kendall + Kylie^TM](https://instagram.com/kendallandkylie/)\n
+"""
+
+KylieJenner = ("kylie", "jenner")
+KylieJennerReply = """
+[Instagram](https://instagram.com/kyliejenner)\n
+[Twitter](https://twitter.com/kyliejenner)\n
+[Kendall + Kylie^TM](https://instagram.com/kendallandkylie/)\n
+"""
+
+ChloeBennet = ("chloe", "bennet")
+ChloeBennetReply = """
+[Instagram](https://instagram.com/chloebennet4/)\n
+[Twitter](https://twitter.com/ChloeBennet4)\n
+"""
+
+HayleyAtwell = ("hayley", "atwell")
+HayleyAtwellReply = """
+[Instagram](https://instagram.com/realhayleyatwell)\n
+[Twitter](https://twitter.com/HayleyAtwell)\n
+"""
+
+AnnaKendrick = ("anna", "kendrick")
+AnnaKendrickReply = """
+[Instagram](https://instagram.com/annakendrick47/)\n
+[Twitter](https://twitter.com/annakendrick47)\n
+"""
+
+DaniThorne = ("dani", "thorne")
+DaniThorneReply = """
+[Instagram](https://instagram.com/dani_thorne/)\n
+[Twitter](https://twitter.com/Dani_Thorne)\n
+[Tumblr](http://danithornesworld.com/)\n
+[Youtube](https://www.youtube.com/user/danithornesworld)\n
+[IMDb](http://www.imdb.com/name/nm2374574/)\n
+[Facebook](https://www.facebook.com/official.danimthorne)\n
+"""
+
+BellaThorne = ("bella", "thorne")
+BellaThorneReply = """
+[Instagram](https://instagram.com/bellathorne)\n
+[Twitter](https://twitter.com/BELLATHORNE)\n
+[IMDb](http://www.imdb.com/name/nm2254074/)\n
+"""
+
+EmiliaClarke = ("emilia", "clarke")
+EmiliaClarkeReply = """
+[Instagram](https://instagram.com/emilia_clarke/)\n
+[Twitter](https://twitter.com/Emilia_Clarke)\n
+"""
+
+JessicaBiel = ("jessica", "biel")
+JessicaBielReply = """
+[Instagram](https://instagram.com/jessicabiel)\n
+[Twitter](https://twitter.com/JessicaBiel)\n
+[WhoSay](http://www.whosay.com/jessicabiel)\n
+"""
+
+AshleyBenson = ("ashley", "benson")
+AshleyBensonReply = """
+[Instagram](https://instagram.com/itsashbenzo)\n
+[Twitter](https://twitter.com/AshBenzo)\n
+[Website](http://ashleybenson.net/)\n
+"""
+
+MelissaBenoist = ("melissa", "benoist")
+MelissaBenoistReply = """
+[Instagram](https://instagram.com/melissabenoist/)\n
+[Twitter](https://twitter.com/MelissaBenoist)\n
+"""
+
+MilanaVayntrub = ("milana", "vayntrub")
+MilanaVayntrubReply = """
+[Instagram](https://instagram.com/mintmilana)\n
+[Twitter](https://twitter.com/MintMilana)\n
+[YouTube: Live Prude Girls](https://www.youtube.com/user/LivePrudeGirls)\n
+"""
+
+HeidiKlum = ("heidi", "klum")
+HeidiKlumReply = """
+[Instagram](https://instagram.com/HeidiKlum)\n
+[Twitter](https://twitter.com/heidiklum/)\n
+[Website](http://www.heidiklum.com/)\n
+"""
+
+StanaKatic = ("stana", "katic")
+StanaKaticReply = """
+[Instagram](https://instagram.com/drstanakatic)\n
+[Twitter](https://twitter.com/stana_katic)\n
+[Website](http://www.stanakatic.com/)\n
+"""
+
+BlakeLively = ("blake", "lively")
+BlakeLivelyReply = """
+[Instagram](https://instagram.com/blakelively/)\n
+[Twitter](https://twitter.com/blakelively)\n
+"""
+
+MelissaDebling = ("melissa", "debling")
+MelissaDeblingReply = """
+[Instagram](https://instagram.com/melissadebling/)\n
+[Twitter](https://www.twitter.com/MelissaDebling)\n
+[Website](http://melissad.co.uk/)\n
+"""
+
+SarahShahi = ("sarah", "shahi")
+SarahShahiReply = """
+[Instagram](https://instagram.com/theonlysarahshahi/)\n
+[Twitter](https://twitter.com/onlysarahshahi)\n
+[WhoSay](http://www.whosay.com/sarahshahi)\n
+"""
+
+celebs = ({SelenaGomez: SelenaGomezReply}, {AlexisRen: AlexisRenReply}, {TaylorSwift: TaylorSwiftReply}, {McKaylaMaroney: McKaylaMaroneyReply},\
+{SarahHyland: SarahHylandReply}, {ArielWinter: ArielWinterReply}, {KarlieKloss: KarlieKlossReply}, {KendallJenner: KendallJennerReply},\
+{KylieJenner: KylieJennerReply}, {ChloeBennet: ChloeBennetReply}, {HayleyAtwell: HayleyAtwellReply}, {AnnaKendrick: AnnaKendrickReply},\
+{DaniThorne: DaniThorneReply}, {BellaThorne: BellaThorneReply}, {EmiliaClarke: EmiliaClarkeReply}, {JessicaBiel: JessicaBielReply},\
+{AshleyBenson: AshleyBensonReply}, {MelissaBenoist: MelissaBenoistReply}, {MilanaVayntrub: MilanaVayntrubReply}, {HeidiKlum: HeidiKlumReply},\
+{StanaKatic: StanaKaticReply}, {BlakeLively: BlakeLivelyReply}, {MelissaDebling: MelissaDeblingReply}, {SarahShahi: SarahShahiReply})
+
+#In this string put all+your+target_subreddits:
+subreddits_string = "celeb_username_bot+Celeb_Bot_Test+DrewsephA"
+
+#When the script is ready delete the above two lines and remove the "#" from the line below
+#subreddits_string = "Celebs+CelebGfys+celebgifs+CelebsWallpaper+goddesses+VSModels+vsangels+Models+PrettyGirls+GirlswithGlasses+GirlswithGreenEyes+GirlswithWetHair+gmbwallpapers+nsfw_phone_wallpapers+swimsuit2015+VictoriaSecret+VictoriasSecret+VSfans+WtSSTaDaMiT+starlets+StarletsWallpaper+girlsinyogapants+girlsinyogashorts"
+
+''' ---------------------------------------------------------------------------------------------------------------- '''
