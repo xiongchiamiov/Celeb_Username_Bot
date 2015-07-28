ab6753c9308b5d98553d002949ccd0c8c0e1ba9f
Drew Allen <drewlallen@gmail.com>
2015-07-28 14:05:50 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-REDDIT_USERAGENT = "<celeb social media helper> 1.0 by /u/DrewsephA & /u/_inu"
+REDDIT_USERAGENT = "<celeb social media helper> 1.0 by /u/DrewsephA"
 REDDIT_USERNAME = ""
 REDDIT_PASSWORD = ""
